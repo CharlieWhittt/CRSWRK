c086eb8955e2d25803b33a1ce1a2c35fe94648af
Charlie <Charlie@CHARLIE-PC>
2018-09-06 23:05:30 +0100

    Add project files.

---
+++ bootstrap/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using bootstrap.Models;
+
+namespace bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DingerDataContext Db = new DingerDataContext();
+
+            IndexModel Model = new IndexModel();
+
+            Model.PT = new List<getPlayersWithTeamResult>();
+
+            Model.PT = Db.getPlayersWithTeam().ToList();
+
+            return View(Model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ bootstrap/Models/PlayerModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using bootstrap.Models;
+
+namespace bootstrap.Models
+{
+    public class IndexModel
+    {
+        public List<getPlayersWithTeamResult> PT { get; set; }
+
+
+    }
+}
